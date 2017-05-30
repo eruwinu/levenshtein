c8076581a004663261e3c1c6f331c70631575a83
Erwin Villejo <erwin.villejo@gmail.com>
2017-05-31 01:33:37 +0800

    add tests

---
+++ levenshtein.cpp
@@ -0,0 +1,5 @@
+#include "levenshtein.hpp"
+
+std::size_t levenshteinDistance(const std::string& s, const std::string& t)
+{
+}
---
+++ levenshtein.hpp
@@ -0,0 +1,5 @@
+#pragma once
+
+#include <string>
+
+std::size_t levenshteinDistance(const std::string&, const std::string&);
